50df4649be03e439491b511e262203a8ade3b820
Anna Zach <anza2501@student.miun.se>
2026-04-17 15:40:13 +0200

    Konverter sida skapad

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -2,8 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
+  standalone: true,
   imports: [],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
-export class About {}
+export class About {
+  name: string = 'Anna';
+  email: string = 'anna@example.com';
+  isActive: boolean = true;
+}
--- src/app/pages/converter/converter.ts
+++ src/app/pages/converter/converter.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-converter',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './converter.html',
   styleUrl: './converter.css',
 })
-export class Converter {}
+export class Converter {
+
+  meters: number = 0;
+  feet: number = 0;
+
+  celsius: number = 0;
+  fahrenheit: number = 0;
+
+  convertMetersToFeet() {
+    this.feet = this.meters * 3.28084;
+  }
+
+  convertCelsiusToFahrenheit() {
+    this.fahrenheit = (this.celsius * 9 / 5) + 32;
+  }
+}
+
